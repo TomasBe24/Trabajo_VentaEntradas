d1d848af546b663c99dea5a3cb6fe8a57778d3dc
TomasBe24 <tomas.berias1@hotmail.com>
2021-09-28 19:32:15 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Trabajo_VentaEntradas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
